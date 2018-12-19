be8c161879acc775b25433a6df5c20a2250f3b01
crystalDf <chendong333@gmail.com>
2018-12-19 22:59:44 +0800

    JUCSampleSemaphore

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
         BoundedResource boundedResource = new BoundedResource(3);
 
         for (int i = 0; i < 10; i++) {
-
             new UserThread(boundedResource).start();
         }
     }
